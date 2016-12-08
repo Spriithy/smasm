3b4161c50b0f3bf2c713defcf2e9ab77ccd475e0
Spriithy <theo.dano@gmail.com>
2016-12-08 15:39:07 +0100

    removed old code generation

--- src/asm/gen.c
+++
@@ -1,9 +0,0 @@
-#include "gen.h"
-
-void gen_sym(FILE *f, int sym) {
-    gen_instr(f, sym, 0);
-}
-
-void gen_instr(FILE *f, int op, int extra) {
-    fprintf(f, "%02X %02X %02X\n", op, (extra >> 8) & 0xff, extra & 0xff);
-}
